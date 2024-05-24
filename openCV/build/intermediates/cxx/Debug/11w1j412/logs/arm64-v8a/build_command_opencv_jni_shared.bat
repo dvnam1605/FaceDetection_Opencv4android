25f223f45dfd2abb62a44a07af265bac03bf4a4d
Pi <ductrung27.dt@gmail.com>
2024-05-24 11:52:01 +0700

    .gitignore update

--- openCV/build/intermediates/cxx/Debug/11w1j412/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"D:\\App\\Android_SDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\Project\\CameraApp\\openCV\\.cxx\\Debug\\11w1j412\\arm64-v8a" ^
-  opencv_jni_shared
--- openCV/build/intermediates/cxx/Debug/11w1j412/logs/x86_64/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\Owner\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "E:\\Study\\Code\\Java\\FaceDetection_Opencv4android\\openCV\\.cxx\\Debug\\11w1j412\\x86_64" ^
-  opencv_jni_shared
